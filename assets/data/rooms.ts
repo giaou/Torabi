3f2dcdc965cb95ce97cb78fa5d3f591c91f66743
Giao U <lesliegiao.u@gmail.com>
2024-07-06 20:00:50 -0500

    Initialize the project

---
+++ assets/data/rooms.ts
@@ -0,0 +1,22 @@
+const rooms = [
+  {
+    id: 1,
+    name: "Regina",
+    price: 345,
+  },
+  {
+    id: 2,
+    name: "Rex",
+    price: 450,
+  },
+  {
+    id: 3,
+    name: "Primis",
+    price: 360,
+  },
+  {
+    id: 4,
+    name: "Ultimas",
+    price: 1903,
+  },
+];
